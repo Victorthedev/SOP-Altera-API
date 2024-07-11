ea75f55d427ad547a2434c62c14e3c846a125e92
Victor Ubahakwe <ubahakweemeka@gmail.com>
2024-07-11 21:51:26 +0100

    Update sopRoutes.js

--- routes/sopRoutes.js
+++ routes/sopRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const sopController = require('../controllers/sopControllers');
 
-// Routes
 router.post('/sops', sopController.createSOP);
 router.get('/sops/:id', sopController.getSOP);
 router.post('/procedures', sopController.logProcedure);
